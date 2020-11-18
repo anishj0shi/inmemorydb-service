a708cf9adf5f549f70d7960ec576ccf8e7cba819
Anish Joshi <anish.joshi@sap.com>
2020-11-18 02:25:54 +0100

    Adding Service HAndlers

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	"context"
-	cloudevents "github.com/cloudevents/sdk-go"
+	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"log"
+	"net/http"
 )
 
-func main()  {
-	c, err := cloudevents.NewDefaultClient()
+func main() {
+	protocol, err := cloudevents.NewHTTP()
+
 	if err != nil {
-		log.Fatalf("failed to create client, %v", err)
+		log.Fatalf("failed to create handler: %s", err.Error())
 	}
-	log.Fatal(c.StartReceiver(context.Background(), receive));
+	h, err := cloudevents.NewHTTPReceiveHandler(context.Background(), protocol, receive)
+	if err != nil {
+		log.Fatalf("failed to create handler: %s", err.Error())
+	}
+
+	log.Printf("will listen on :8081\n")
+	go func() {
+		if err := http.ListenAndServe(":8081", h); err != nil {
+			log.Fatalf("unable to start http server, %s", err)
+		}
+	}()
+
+	log.Print("Starting Result Service")
+
+	mux := http.NewServeMux()
+	mux.Handle("/", middleware())
+	if err := http.ListenAndServe(":8082", mux); err != nil {
+		log.Fatalf("unable to start http server for Result Service, %s", err)
+	}
 }
 
 func receive(event cloudevents.Event) {
 	log.Printf("%s", event)
+}
+
+func middleware() http.Handler {
+	return &handler{}
+}
+
+type handler struct{}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Get was triggered"))
+
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/anishj0shi/inmemorydb-service
 go 1.15
 
 require (
-	github.com/cloudevents/sdk-go v1.2.0
+	github.com/cloudevents/sdk-go/v2 v2.3.1
 	github.com/hashicorp/go-memdb v1.3.0
 )
